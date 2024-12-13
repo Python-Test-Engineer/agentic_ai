4e4ad29a9fc841276e2a18dfda6a656b7cc8deb6
Craig West <iwswordpress@gmail.com>
2024-12-13 18:43:45 +0000

    class|_agent

---
+++ 05_agent_class.py
@@ -0,0 +1,134 @@
+import os
+import json
+from dotenv import load_dotenv
+from openai import OpenAI
+import anthropic
+import requests
+from rich.console import Console
+
+console = Console()
+
+# Load environment variables in a file called .env
+# Print the key prefixes to help with any debugging
+
+load_dotenv()
+openai_api_key = os.getenv("OPENAI_API_KEY")
+anthropic_api_key = os.getenv("ANTHROPIC_API_KEY")
+# google_api_key = os.getenv('GOOGLE_API_KEY')
+
+if openai_api_key:
+    print(f"OpenAI API Key exists and begins {openai_api_key[:8]}")
+else:
+    print("OpenAI API Key not set")
+
+if anthropic_api_key:
+    print(f"Anthropic API Key exists and begins {anthropic_api_key[:7]}")
+else:
+    print("Anthropic API Key not set")
+
+
+openai = OpenAI()
+claude = anthropic.Anthropic()
+
+
+# original propmt
+system_message = """
+You are an assistant that is very good at determining what tool to use to solve queries.
+"""
+
+ai_programming = """
+
+TOOLS
+
+You have two tools:
+
+1. A calculator tool that can perform basic arithmetic operations, such as addition, subtraction, multiplication, and division. If this tool is used then respond as detailed in the OUTPUT section with "next": "do_calculation"
+2. A jokes tool that can provide a light-hearted joke for the audience reqested. If this tool is used then respond as detailed in <OUTPUT> with "next": "do_joke" and "audience": "audience requested"
+
+
+OUTPUT
+
+For a user query, determine the best tool. If none of the tools are appropriate, respond with "None".
+
+EXAMPLES
+
+    First Example
+    
+    |{"tool": "calculator", "next": "do_calculation", "arguments": {"num1": 5, "num2": 5, "operation": "addition"}}|
+
+    Second Example
+
+    
+    |{"tool": "joke", "next": "do_joke", "audience": "Pythonistas" }}|
+    
+Thank you for your help.
+"""
+
+system_message += ai_programming
+
+
+user_prompt = "What is 5 times 9?"
+# user_prompt = "Tell me a joke when I am doing stand up at a Builders Conference"
+
+# user_prompt = "Tell me a joke when I am doing stand up at a Builders Conference"
+
+prompts = [
+    {"role": "system", "content": system_message},
+    {"role": "user", "content": user_prompt},
+]
+
+
+class Agent:
+    def __init__(self, model="gpt-3.5-turbo", prompts=prompts):
+        self.model = model
+        self.prompt = prompts
+
+    def get_tool(self):
+        response = openai.chat.completions.create(
+            model=self.model, messages=self.prompt
+        )
+        output = response.choices[0].message.content.replace("\n", "")
+        json_output = output.strip()
+        json_output = json.loads(json_output)
+        # print(json_output)
+        return json_output
+
+    def do_calculation(self, num1, num2, operation):
+        if operation == "addition":
+            return num1 + num2
+        elif operation == "subtraction":
+            return num1 - num2
+        elif operation == "multiplication":
+            return num1 * num2
+        elif operation == "division":
+            return num1 / num2
+
+    def do_joke(self, audience):
+        return f"Joke for {audience}"
+
+    def give_answer(self):
+        output = self.get_tool()
+        print(type(output), output)
+
+        if output["next"] == "do_calculation":
+            num1 = output["arguments"]["num1"]
+            num2 = output["arguments"]["num2"]
+            operation = output["arguments"]["operation"]
+            answer = self.do_calculation(num1, num2, operation)
+            return answer
+        elif output["next"] == "do_joke":
+            audience = output["audience"]
+            answer = self.do_joke(audience)
+            return answer
+
+
+agent = Agent()
+output = agent.get_tool()
+# print(output)
+
+
+answer = agent.give_answer()
+answer_text = f"[dark_orange]Answer for '{user_prompt}':[/] [green]{answer}[/]"
+console.print(answer_text)
+
+
